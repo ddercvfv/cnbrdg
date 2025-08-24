6c9c529e8b50c0ca4d34c11b1cb51d0eaca84fab
ddercvfv <ripsat91@gmail.com>
2025-08-24 08:26:25 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,11 +12,7 @@ ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))  # ID администратора д
 # Ссылки и данные
 REVIEWS_CHANNEL = "https://t.me/estasiacars"
 COURSE_POST_LINK = "https://t.me/cnchange/185"
-YANDEX_MAPS_LINK = "https://yandex.ru/maps/org/cn_bridge/37529426458/reviews/?from=mapframe&ll=37.625325%2C55.695281&source=mapframe&tab=reviews&um=constructor%3Ab4c9ca86698ace75ba702643723eee2c41f695b5c08a869315a7e1a26937c4bb&utm_source=mapframe&z=15.4"
-TELEGRAM = "https://t.me/cnbridgeru"
-INSTARGRAM = "https://www.instagram.com/cn.bridge"
-VK = "https://vk.com/cnbridge"
-DZEN = "https://dzen.ru/id/67485ea7f18c29468e3620c4?share_to=link"
+YANDEX_MAPS_LINK = "https://yandex.ru/maps/-/CHx3IY9h"
 
 # Социальные сети
 SOCIAL_LINKS = {
